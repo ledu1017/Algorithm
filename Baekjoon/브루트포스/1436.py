f880f95f4c603a6888ae7c4f0ed4a5c30c87f133
ledu1017 <ledu202@naver.com>
2025-06-26 18:31:52 +0900

    브루트포스

---
+++ Baekjoon/브루트포스/1436.py
@@ -0,0 +1,11 @@
+num = input()
+count = 0
+title = 0
+
+while True:
+    if '666' in str(title):
+        count += 1
+        if count == int(num):
+            print(title)
+            break
+    title += 1

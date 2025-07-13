1cde0757c35747b5a26f1ecf56cb6287a258146e
ledu1017 <ledu202@naver.com>
2025-07-13 15:35:12 +0900

    동적

---
+++ Baekjoon/동적 계획법 1/24416(피보나치_수_1).py
@@ -0,0 +1,14 @@
+import sys
+
+def fib():
+    n_list .append(n_list[-1] + n_list[-2])
+    return n_list[-1]
+
+n_list = [1, 1]
+n = int(sys.stdin.readline().strip())
+result, count = 0, 0
+
+for _ in range(n -2):
+    fib()
+
+print(n_list[-1], n - 2)
